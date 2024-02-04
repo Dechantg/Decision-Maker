83d6a47aa84326511a2be6933ea979c16b8e04f2
Gregory Dechant <dechantg@gmail.com>
2024-02-03 19:28:24 -0800

    deleted some unneed commented out lines

--- routes/logout.js
+++ routes/logout.js
@@ -2,17 +2,10 @@
 const express         = require('express');
 const router          = express.Router();
 const session         = require('express-session');
-const bcrypt          = require('bcrypt');
 
-const userQueries     = require('../db/queries/users');
-const uuid            = require('uuid');
 
-const userExists      = require('../db/queries/user_exists');
 
-const addUser         = require('../db/queries/add_user')
-const bodyParser      = require('body-parser');
 
-router.use(bodyParser.json());
 
 
 router.post('/', (req, res) => {
--- server.js
+++ server.js
@@ -30,12 +30,6 @@ app.use(session({
 }));
 
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
 app.use(express.urlencoded({ extended: true }));
